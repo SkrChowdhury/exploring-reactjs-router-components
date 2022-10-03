f26f1beecb4ad19838db0f083c63c53a8ac99d16
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-03 17:30:09 +0600

    Browser Router, Routes and Route Component

---
+++ src/components/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>This Is The Products Component</h2>
+            
+        </div>
+    );
+};
+
+export default Products;
